130b4e4ed8cdd3d8b0d27f2226e3221a9722213f
jeandejesus <jeanfreirecontato@outlook.com>
2025-08-26 11:23:41 -0300

    ajustes de cores e designer

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 // src/app/shared/navbar/navbar.component.ts
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, Router, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
@@ -16,6 +16,21 @@ export class NavbarComponent {
   isMenuOpen = false;
 
   constructor(private authService: AuthService, private router: Router) {}
+
+  @ViewChild('navMenu') navMenu!: ElementRef;
+
+  @ViewChild('hamburgerButton') hamburgerButton!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event): void {
+    if (
+      this.isMenuOpen &&
+      !this.navMenu.nativeElement.contains(event.target) &&
+      !this.hamburgerButton.nativeElement.contains(event.target)
+    ) {
+      this.isMenuOpen = false; // Fecha o menu
+    }
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
