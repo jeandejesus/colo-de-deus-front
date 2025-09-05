2d9cd167124df4d196622bf41afe8bce45641b28
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-04 22:22:33 -0300

    update style income, expenses and dashbord

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,1 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://colo-de-deus-api.onrender.com',
-  vapidPublicKey:
-    'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
-};
+export const environment = {};
