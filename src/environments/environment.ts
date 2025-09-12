0a333b48f2be92b92855212da83a3768e1b16aaf
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-12 12:07:46 -0300

    send envrionment to test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000',
+  apiUrl: '/api',
+  production: true,
   vapidPublicKey:
     'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
 };
