bc053532d3c8f18531618976736aa526bc239bce
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-12 00:09:46 -0300

    fix envirionment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:3000',
   vapidPublicKey:
     'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
 };
-=======
-export const environment = {};
->>>>>>> d9b050ea3775e6b87499a08305e420f9695ac805
