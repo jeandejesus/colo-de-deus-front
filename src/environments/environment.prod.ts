1e9af2492b01e22623a8d02c88fac07e4782c3a4
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-12 12:02:03 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  apiUrl: '/api',
   production: true,
-  vapidPublicKey:
-    'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
+  apiUrl: 'https://api.colodedeuscwb.com.br', // <- usar HTTPS e domínio real
+  vapidPublicKey: 'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
 };
