b947d36d3a60514b631828c5af5f9416c119dc25
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-12 12:16:55 -0300

    updade apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: '/api',
+  apiUrl: 'https://api.colodedeuscwb.com.br',
   production: true,
   vapidPublicKey:
     'BODD-W0-GH3GOhbgqjNhPBSLQW6q5YohLi3Wq7dTCgyevsp5uQwri8SW9p0vt0ccL2WNpzimJ3oGX6JnrRWqoUM',
