bf8b57726e3b5ca069127203cfd0cbad839f0bcd
jeandejesus <jeanfreirecontato@outlook.com>
2025-09-15 18:41:30 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://api.colodedeuscwb.com.br',
   production: true,
   vapidPublicKey:
     'BASfQDTyjqV1G7eH2v5VGzvpjf131uRbuNe6SRP2XedL1IKsm6SctOq3urYTrprDNefBXU6tIZFE_xwGwnK_JhI',
