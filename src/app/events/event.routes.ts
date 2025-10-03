bf4645f8b576cfd1680d9c61d4fc385636565ab7
jeandejesus <jeanfreirecontato@outlook.com>
2025-10-03 18:08:37 -0300

    checkin de eventos

--- src/app/events/event.routes.ts
+++ src/app/events/event.routes.ts
@@ -5,13 +5,15 @@ import { EventFormComponent } from './pages/event-form/event-form.component';
 import { EventRegisterComponent } from './pages/event-register/event-register.component';
 import { UserEventListComponent } from './pages/user-event-list/user-event-list.component';
 import { UserEventQRCodeComponent } from './pages/user-event-qr/user-event-qr.component';
+import { EventCheckinComponent } from './pages/event-checkin/event-checkin.component';
 
 export const EVENT_ROUTES: Routes = [
   { path: '', component: EventListComponent },
   { path: 'list', component: UserEventListComponent },
-  { path: 'my-event/:id/qr', component: UserEventQRCodeComponent }, // específico
+  { path: 'my-event/:id/qr', component: UserEventQRCodeComponent },
+  { path: 'checkin', component: EventCheckinComponent }, // rota do leitor QR
   { path: 'create', component: EventFormComponent },
   { path: 'edit/:id', component: EventFormComponent },
-  { path: ':id/register', component: EventRegisterComponent }, // genérico, mas ainda específico
-  { path: ':id', component: EventDetailComponent }, // sempre por último
+  { path: ':id/register', component: EventRegisterComponent },
+  { path: ':id', component: EventDetailComponent }, // genérica SEMPRE por último
 ];
